11d6d984aa2cdaea6baa6a35be024b01a7302039
Trevor Bramwell <trevor@bramwell.net>
2015-05-18 11:41:42 -0700

    Bump to 0.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+code_generator CHANGELOG
+========================
+This file is used to list changes made in each version of the
+code_generator cookbook.
+
+0.2.0
+-----
+- Style and standards updates to code_generator
+- Rakefile for generating Snakeoil SSL Certificates
+- Added rubocop and tests for new cookbooks
+
+
+0.1.0
+-----
+- Initial release of code_generator
+
--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'code_generator'
 maintainer       'Oregon State University'
-maintainer_email 'systems@osuosl.org'
+maintainer_email 'chef@osuosl.org'
 license          'All Rights Reserved'
 description      'Generates Chef code for the OSUOSL'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
