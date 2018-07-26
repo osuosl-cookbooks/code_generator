d64f06e3c888dba32d8060710ee74b7be4b08576
Lance Albertson <lance@osuosl.org>
2018-07-26 14:30:28 -0700

    More Chef 13 fixes

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,9 @@ maintainer       'Oregon State University'
 maintainer_email 'chef@osuosl.org'
 source_url       'https://github.com/osuosl-cookbooks/code_generator'
 issues_url       'https://github.com/osuosl-cookbooks/code_generator/issues'
-license          'All Rights Reserved'
+license          'Apache-2.0'
+chef_version     '>= 12.18' if respond_to?(:chef_version)
 description      'Generates Chef code for the OSUOSL'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.8'
+supports         'centos', '~> 7.0'
